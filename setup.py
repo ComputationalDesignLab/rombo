468990bed3eb606121d4b4050d30485e22c665f3
Abhijnan03 <abhijnan2000@gmail.com>
2024-10-10 12:39:43 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='aromad',
     version='0.1.0',
     packages=find_packages(include=['aromad*']),
-    install_requires=["numpy>=1.21.6", "scipy>=1.7.3", "smt>=2.0.0", "botorch"],
+    install_requires=["numpy>=1.21.6", "scipy>=1.7.3", "botorch>=0.8.0"],
 )
