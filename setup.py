fe8f05cbb9616362054db482dc5f4c628e213c37
Abhijnan03 <abhijnan2000@gmail.com>
2024-11-04 14:11:25 -0500

    Updated name of package

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aromad',
+    name='rombo',
     version='0.1.0',
-    packages=find_packages(include=['aromad*']),
+    packages=find_packages(include=['rombo*']),
     install_requires=["numpy>=1.21.6", "scipy>=1.7.3", "botorch>=0.8.0"],
 )
