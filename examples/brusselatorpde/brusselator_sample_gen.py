277bf01467f922c924a29c32c49484b27029d30a
Abhijnan03 <abhijnan2000@gmail.com>
2024-11-04 17:55:31 -0500

    Updated Brusselator test case

--- examples/brusselatorpde/brusselator_sample_gen.py
+++ examples/brusselatorpde/brusselator_sample_gen.py
@@ -7,15 +7,24 @@ from smt.sampling_methods import LHS
 tkwargs = {"device": torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0"), "dtype": torch.float}
 
 # Defining the problem
-problem = BrusselatorPDE(input_dim=4, Nx=64, Ny=64, tkwargs=tkwargs)
+input_dim = 128
+problem = BrusselatorPDE(input_dim=input_dim, Nx=64, Ny=64, tkwargs=tkwargs)
+
+# Generating data for multiple trials of the Brusselator PDE method
 
 # Generating the design of experiments
-n_data = 50
-xlimits = np.array([[0.0,1.0]]*32)
-sampler = LHS(xlimits=xlimits, criterion="ese")
-xtrain = sampler(n_data)
-ytrain = problem.evaluate(xtrain)
+n_data = 15
+n_trials = 20
+xtrain = np.zeros((n_trials, n_data, input_dim))
+ytrain = torch.zeros((n_trials, n_data, 2, problem.Nx, problem.Ny))
+for trial in range(n_trials):
+    print("\n\n### Running trial {} ###".format(trial+1))
+    xlimits = np.array([[0.0,1.0]]*input_dim)
+    sampler = LHS(xlimits=xlimits, criterion="ese")
+    X = sampler(n_data)
+    xtrain[trial,:,:] = X
+    ytrain[trial,:,:,:,:] = problem.evaluate(X)
 
 # Saving the samples generated
 samples = {"x": torch.tensor(xtrain, **tkwargs), "y": ytrain}
-savemat("bruss_data_50.mat", samples)
+savemat("bruss_data_multrial.mat", samples)
