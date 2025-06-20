a29be38aa2e485132d49988ec9a14d3305ae9645
Abhijnan03 <abhijnan2000@gmail.com>
2025-06-20 19:50:40 -0400

    added IBNN examples for brusselator and langermann

--- examples/brusselatorpde/brusselator_ibnn.py
+++ examples/brusselatorpde/brusselator_ibnn.py
@@ -5,40 +5,39 @@ from smt.sampling_methods import LHS
 from rombo.rom.nonlinrom import AUTOENCROM
 import numpy as np
 from rombo.dimensionality_reduction.autoencoder import MLPAutoEnc
-from rombo.test_problems.test_problems import EnvModelFunction
+from rombo.test_problems.test_problems import BrusselatorPDE
 from rombo.optimization.rombo import ROMBO
 from rombo.optimization.stdbo import BO
 from botorch.models.transforms import Standardize
-from scipy.io import savemat, loadmat
 import argparse
+from scipy.io import savemat, loadmat
 import warnings
 warnings.filterwarnings('ignore')
 
 # Importing relevant classes from BoTorch
 from botorch.acquisition import qExpectedImprovement, qLogExpectedImprovement
-from botorch.models import KroneckerMultiTaskGP, SingleTaskGP
 from botorch.models.kernels import InfiniteWidthBNNKernel
+from botorch.models import KroneckerMultiTaskGP, SingleTaskGP
 from gpytorch.mlls import ExactMarginalLogLikelihood
 
 tkwargs = {"device": torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0"), "dtype": torch.float64}
 
-# Parsing parameters from command line
+# Parsing parameters from the command line
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_dim", help="input dimension of the function", type=int)
-parser.add_argument("--output_dim", help="output dimension of the function", type=int)
+parser.add_argument("--input_dim", help="input dimension of the function",type=int)
 parser.add_argument("--latent_dim", help="latent dimension of the model", type=int)
 parser.add_argument("--mc_samples", help="number of MC samples", type=int)
-parser.add_argument("--trial_num", help="number of the trial of the program",type=int)
+parser.add_argument("--trial_num", help="number of the trial run",type=int)
 args = parser.parse_args()
 
-# Instantiating the problem and defining optimization parameters
+# Instantiating the problem and setting the optimization parameters
 inputdim = args.input_dim
 xlimits = np.array([[0.0, 1.0]]*inputdim)
-n_init = 10
-objective = EnvModelFunction(input_dim=inputdim, output_dim=args.output_dim, normalized=True)
+n_init = 15
+objective = BrusselatorPDE(input_dim=inputdim, Nx=64, Ny=64)
 bounds = torch.cat((torch.zeros(1, inputdim), torch.ones(1, inputdim))).to(**tkwargs)
 n_trials = 1
-n_iterations = 40
+n_iterations = 25
 
 # Defining arrays to store values during the optimization loop
 boei_objectives = np.zeros((n_trials, n_iterations))
@@ -61,6 +60,11 @@ bologei_doe = np.zeros((n_trials, n_iterations+n_init, inputdim))
 romboei_doe = np.zeros((n_trials, n_iterations+n_init, inputdim))
 rombologei_doe = np.zeros((n_trials, n_iterations+n_init, inputdim))
 
+boei_predictions = np.zeros((n_trials, n_iterations))
+bologei_predictions = np.zeros((n_trials, n_iterations))
+romboei_predictions = np.zeros((n_trials, n_iterations, 8192))
+rombologei_predictions = np.zeros((n_trials, n_iterations, 8192))
+
 boei_EI = np.zeros((n_trials, n_iterations))
 bologei_EI = np.zeros((n_trials, n_iterations))
 romboei_EI = np.zeros((n_trials, n_iterations))
@@ -70,8 +74,8 @@ for trial in range(n_trials):
 
     print("\n\n##### Running trial {} out of {} #####".format(trial+1, n_trials))
 
-    # Generating the initial sample for the trial
-    sampler = LHS(xlimits=xlimits, criterion="ese", random_state=args.trial_num)
+    # Generating the initial design of experiments
+    sampler = LHS(xlimits=xlimits, criterion="ese", random_state = args.trial_num)
     xdoe = sampler(n_init)
     xdoe = torch.tensor(xdoe, **tkwargs)
     ydoe = objective.evaluate(xdoe)
@@ -81,7 +85,7 @@ for trial in range(n_trials):
     score_doe = objective.utility(ydoe).unsqueeze(-1)
 
     # Definition the BO optimizers
-    autoencoder = MLPAutoEnc(high_dim=ydoe.shape[-1], hidden_dims=[256,64], zd = args.latent_dim, activation = torch.nn.SiLU())
+    autoencoder = MLPAutoEnc(high_dim=objective.output_dim, hidden_dims=[512,128,64], zd = args.latent_dim, activation = torch.nn.SiLU())
     autoencoder.double()
     rom_args = {"autoencoder": autoencoder, "low_dim_model": KroneckerMultiTaskGP, "low_dim_likelihood": ExactMarginalLogLikelihood,
                 "standard": False, "saas": False, "ibnn": True, "ibnn_depth": 4}
@@ -131,14 +135,15 @@ for trial in range(n_trials):
         rombologei_objectives[trial][iteration] = optimizer1.best_f
         rombologei_dvs[trial][iteration] = optimizer1.best_x
         rombologei_EI[trial][iteration] = optimizer1.maxEI
-    
+
     boei_doe[trial] = optimizer3.xdoe.detach().cpu().numpy()
     bologei_doe[trial] = optimizer4.xdoe.detach().cpu().numpy()
     romboei_doe[trial] = optimizer2.xdoe.detach().cpu().numpy()
     rombologei_doe[trial] = optimizer1.xdoe.detach().cpu().numpy()
- 
-# Storing the final data
-results = {"BO_EI": {"objectives": boei_objectives, "design": boei_dvs, "doe": boei_doe, "time": boei_t}, "BO_LOGEI": {"objectives": bologei_objectives, "design": bologei_dvs, "doe": bologei_doe, "time": bologei_t}, 
-           "ROMBO_EI": {"objectives": romboei_objectives, "design": romboei_dvs, "doe": romboei_doe, "time": romboei_t}, "ROMBO_LOGEI": {"objectives": rombologei_objectives, "design": rombologei_dvs, "doe": rombologei_doe, "time": rombologei_t}}
-#savemat("./env_model_results_{}_{}_IBNN_trial_{}_LD_{}_MC_{}.mat".format(args.input_dim, args.output_dim, args.trial_num, args.latent_dim, args.mc_samples), results)
 
+# Saving the final data
+results = {"BO_EI": {"objectives": boei_objectives, "design": boei_dvs, "doe": boei_doe, "time": boei_t, "predictions": boei_predictions, "EI": boei_EI}, 
+            "BO_LOGEI": {"objectives": bologei_objectives, "design": bologei_dvs, "doe": bologei_doe, "time": bologei_t, "predictions": bologei_predictions, "EI": bologei_EI}, 
+           "ROMBO_EI": {"objectives": romboei_objectives, "design": romboei_dvs, "doe": romboei_doe, "time": romboei_t, "predictions": romboei_predictions, "EI": romboei_EI}, 
+           "ROMBO_LOGEI": {"objectives": rombologei_objectives, "design": rombologei_dvs, "doe": rombologei_doe, "time": rombologei_t, "predictions": rombologei_predictions, "EI": rombologei_EI}}
+#savemat("./brusselator_results_{}_{}_trial_{}_LD_{}_MC_{}.mat".format(args.input_dim, args.mc_samples, args.trial_num, args.latent_dim, args.mc_samples), results)
