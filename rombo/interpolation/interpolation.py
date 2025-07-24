82d2794b58851b74717dac0f136c353b67454fa5
Abhijnan03 <abhijnan2000@gmail.com>
2025-07-24 14:08:42 -0400

    updated interpolation.py

--- rombo/interpolation/interpolation.py
+++ rombo/interpolation/interpolation.py
@@ -1,15 +1,6 @@
 # Some of the code in this script has been adapted from the BoTorch code and tutorials
 # https://botorch.org/docs/tutorials/, https://botorch.readthedocs.io/en/stable/
 
-# For more details on the implementation of the deep kernel learning model, see the following papers and repositories
-# 1. N. Maus, Z. J. Lin, M. Balandat, E. Bakshy, Joint composite latent
-# space bayesian optimization, in: Forty-first International Conference
-# on Machine Learning, Vienna, Austria, 21-27 July, 2024. Code : https://github.com/nataliemaus/joco_icml24
-
-# 2. GPyTorch Documentation: https://docs.gpytorch.ai/en/v1.13/examples/06_PyTorch_NN_Integration_DKL/KISSGP_Deep_Kernel_Regression_CUDA.html
-
-# 3. BoTorch API Documentation: https://botorch.readthedocs.io/en/latest/_modules/botorch/models/gpytorch.html#GPyTorchModel 
-
 # Importing relevant libraries
 import torch
 import torch.nn as nn
@@ -152,93 +143,6 @@ class BoTorchModelList(MLModel):
         elif return_format == "numpy":
             return predictions.cpu().numpy(), variances.cpu().numpy()
 
-# Definition of the deep kernel learning model
-# This definition is slightly different from the BoTorch models since the model
-# is being built from scratch rather than directly being imported from BoTorch
-class DeepKernelGP(gpytorch.models.ExactGP, MLModel):
-
-    # Need to standardize the output data and add in that capability
-    def __init__(self, train_x, train_y, hidden_dims, zd, outcome_transform, activation=nn.ReLU()):
-
-        # Specifying the likelihood as the Gaussian likelihood
-        super(DeepKernelGP, self).__init__(train_x, train_y, gpytorch.likelihoods.GaussianLikelihood())
-        self._settraindata(train_x, train_y)
-        
-        # Setting the mean and kernel function for the GP model
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-        self.num_outputs = 1
-
-        # Setting the outcome transform for the model
-        self.outcome_transform = outcome_transform
-
-        # Setting up the feature extractor
-        encoder_layers = []
-        last_dim = train_x.shape[-1]
-
-        for dim in hidden_dims:
-            encoder_layers.append(nn.Linear(last_dim, dim))
-            encoder_layers.append(activation)
-            last_dim = dim
-        
-        encoder_layers.append(nn.Linear(last_dim, zd))
-        self.feature_extractor = nn.Sequential(*encoder_layers)
-
-    def forward(self, x):
-
-        projected_x = self.feature_extractor(x) # Projecting the inputs to the latent space
-        mean_x = self.mean_module(projected_x) 
-        covar_x = self.covar_module(projected_x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
-    
-    def posterior(self, X, output_indices=None, observation_noise=False, *args, **kwargs) -> GPyTorchPosterior:
-
-        self.eval()
-        self.likelihood.eval()
-        dist = self.likelihood(self(X))
-        posterior = GPyTorchPosterior(distribution=dist)
-        posterior = self.outcome_transform.untransform_posterior(posterior, X=X)
-        return posterior
-    
-    def trainModel(self):
-
-        if torch.cuda.is_available():
-            self.train().cuda().double() # Set the model in training mode
-            self.likelihood.train().cuda().double()
-        else:
-            self.train().double() # Set the model in training mode
-            self.likelihood.train().double()
-
-        optimizer = torch.optim.Adam([
-            {'params': self.feature_extractor.parameters()},
-            {'params': self.covar_module.parameters()},
-            {'params': self.mean_module.parameters()},
-            {'params': self.likelihood.parameters()},
-        ], lr=0.01)
-
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self)
-
-        for _ in range(1000):
-            optimizer.zero_grad()
-            output = self(self.train_x)
-            loss = -mll(output, self.train_y)
-            loss.backward()
-            optimizer.step()
-
-    def predict(self, xtest, return_format = 'tensor'):
-
-        # Obtaining the posterior distribution of the model
-        posterior = self.posterior(xtest)
-        # Obtaining mean value predictions
-        predictions = posterior.mean
-        # Obtaining variances
-        variances = posterior.variance
-
-        if return_format == "tensor":
-            return predictions, variances
-        
-        elif return_format == "numpy":
-            return predictions.cpu().numpy(), variances.cpu().numpy()
 
 
 
