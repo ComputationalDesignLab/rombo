cc30def53c5798568ca338b97ffa88aa5217b11b
Abhijnan03 <abhijnan2000@gmail.com>
2025-10-14 15:07:03 -0400

    minor update to code comments

--- rombo/interpolation/interpolation.py
+++ rombo/interpolation/interpolation.py
@@ -71,11 +71,11 @@ class BoTorchModel(MLModel):
         return gp
 
     "Method to use fully Bayesian training and No U-Turn sampling"
-    # For the airfoil case, the warmup steps and the number of samples were lowered to enable a faster runtime 
+    # For the airfoil case, the number of warmup steps and the number of samples were lowered to enable a faster runtime 
     # on a computing cluster without GPU acceleration. 
     # As an example, you can set the following to have a more manageable runtime for the airfoil case on a cluster without GPUs: 
     # WARMUP_STEPS=32, NUM_SAMPLES=16, THINNING=16
-    # To compare with the exact performance for the airfoil case, it will be more appropriate to use the data given directly in the 
+    # However, to compare with the exact performance for the airfoil case, it will be more appropriate to use the data given directly in the 
     # final_results.zip folder in the GitHub repository.
     
     def fit_bayesian(self, gp, WARMUP_STEPS=256, NUM_SAMPLES=128, THINNING=16):
